c663aae7ab7974770f127ff4b0f60152c252b63a
denniman <dennimanadams@gmail.com>
2024-04-18 20:05:39 +0100

    feat: update dashboard layout ui

--- src/components/DashboardLayout/DashboardLayout.jsx
+++ src/components/DashboardLayout/DashboardLayout.jsx
@@ -1,41 +1,65 @@
 import PropTypes from "prop-types";
+import { Sidebar } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+    HiUser,
+    HiInbox,
+    HiTable,
+    HiChartPie,
+    HiViewBoards,
+    HiArrowSmRight,
+    HiShoppingBag,
+} from "react-icons/hi";
 
 export const DashboardLayout = ({ children }) => {
     return (
         <div className="flex h-screen">
             {/* Sidebar */}
-            <aside className="w-64 bg-gray-800">
-                <div className="p-4 text-white">
-                    <h1 className="text-xl font-semibold">
-                        E-Commerce Dashboard
-                    </h1>
-                    {/* Sidebar menu */}
-                    <nav className="mt-4">
-                        <ul>
-                            <li className="py-2 hover:bg-gray-700 cursor-pointer">
-                                <a href="#" className="block">
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li className="py-2 hover:bg-gray-700 cursor-pointer">
-                                <a href="#" className="block">
-                                    Orders
-                                </a>
-                            </li>
-                            <li className="py-2 hover:bg-gray-700 cursor-pointer">
-                                <a href="#" className="block">
-                                    Products
-                                </a>
-                            </li>
-                            {/* Add more sidebar items as needed */}
-                        </ul>
-                    </nav>
-                </div>
-            </aside>
+            <div className="w-64 bg-gray-800">
+                {/* Sidebar content */}
+                <Sidebar aria-label="Sidebar with logo branding example">
+                    <Sidebar.Logo href="#" img="/assets/react.svg">
+                        SwitchCommerce
+                    </Sidebar.Logo>
+                    <Sidebar.Items>
+                        <Sidebar.ItemGroup>
+                            <Sidebar.Item icon={HiChartPie}>
+                                <Link to={"/dashboard"}>Dashboard</Link>
+                            </Sidebar.Item>
+                            <Sidebar.Item icon={HiViewBoards}>
+                                <Link to={"/transactions"}>Transaction</Link>
+                            </Sidebar.Item>
+                            <Sidebar.Item icon={HiInbox}>Inbox</Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiUser}>
+                                Customers
+                            </Sidebar.Item>
+                            <Sidebar.Item icon={HiShoppingBag}>
+                                <Link to={"/products"}>Products</Link>
+                            </Sidebar.Item>
+                            <Sidebar.Item icon={HiShoppingBag}>
+                                <Link to={"/store"}>Store</Link>
+                            </Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                                Sign In
+                            </Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiTable}>
+                                Sign Up
+                            </Sidebar.Item>
+                        </Sidebar.ItemGroup>
+                    </Sidebar.Items>
+                </Sidebar>
+            </div>
+
             {/* Main content */}
-            <main className="flex-1 bg-gray-200">
+            <div className="flex-1 bg-gray-100">
+                {/* Top bar */}
+                <div className="bg-white p-4 shadow-md">
+                    {/* Top bar content */}
+                </div>
+
+                {/* Main content area */}
                 <div className="p-4">{children}</div>
-            </main>
+            </div>
         </div>
     );
 };
